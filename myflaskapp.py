706bf7ed5b6acc562f01628b0b47dbcc19063564
40323251 <vup4bp4972218@yahoo.com.tw>
2016-06-02 10:55:08 +0800

    40323251

--- myflaskapp.py
+++ myflaskapp.py
@@ -12,6 +12,7 @@ import users.b.g5.b40323204_cdw11_1
 import users.b.g5.b40323204_cdw11_2
 import users.b.g5.b40323206_cdw11_1
 import users.b.g5.b40323206_cdw11_2
+import users.b.g5.b40323251_cdw11_2
 import users.b.g5.bg5_40323253
 import users.b.g5.bg5_40323253_1
 import users.b.g5.bg5_40323217
@@ -395,6 +396,7 @@ app.register_blueprint(users.b.g5.b40323204_cdw11_2.bg5_40323204_1)
 app.register_blueprint(users.b.g5.b40323204_cdw11.bg5_40323204)
 app.register_blueprint(users.b.g5.b40323206_cdw11_1.bg5_40323206_1)
 app.register_blueprint(users.b.g5.b40323206_cdw11_2.bg5_40323206_2)
+app.register_blueprint(users.b.g5.b40323251_cdw11_2.bg5_40323251_2)
 app.register_blueprint(users.b.g5.bg5_40323253.bg5_40323253)
 app.register_blueprint(users.b.g5.bg5_40323253_1.bg5_40323253_1)
 app.register_blueprint(users.b.g5.bg5_40323217.bg5_40323217)
